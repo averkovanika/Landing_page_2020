b50abbe86c8cfae95147d7c802c3b2c11f6472e9
averkovanika <averkova.nika98@gmail.com>
2021-02-15 12:11:18 +0300

    Landing_page_project

---
+++ js/init.js
@@ -0,0 +1,15 @@
+(function($){
+    $(function(){
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+  
+
+$( document ).ready(function($) {
+  
+});
+
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
+
+
